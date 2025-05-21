51714e9d404440238714a794cc972411b78e2107
Lucas Pedro <lucas.pedro@leoo.com.br>
2025-05-21 16:37:14 -0300

    Alteração no title da primeira página; Adição do logo da Leoo; Alteração na cor de fundo da primeira página;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Widget } from "./components/Widget/Widget";
 import WppButton from "./components/WppButton/WppButton";
+import logoLeooImage from './assets/leoLogo.png';
 
 export function App() {
   return <>
     {/* <WppButton />
     <Widget /> */}
+    <img src={logoLeooImage} alt="Logo Leoo" className="w-[300px] max-w-[90%]" />
   </>
 }
