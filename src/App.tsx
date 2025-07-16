36cb6dde3fcd35f67998a20860fbdc3e2a2fbdfa
Lucas Silva <lucaspedrodeveloper@gmail.com>
2025-07-16 18:44:51 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ export function App() {
         window.embeddedservice_bootstrap.settings.language = "pt_BR";
         // @ts-ignore
         window.embeddedservice_bootstrap.init(
-          "00DHZ000004qvj3",
-          "Bot_Addiante",
-          "https://addiante--devleoo1.sandbox.my.site.com/ESWBotAddiante1742584171174",
+          "00DHr000003YtPL",
+          "Chatbot_Addiante_Site",
+          "https://addiante.my.site.com/ESWChatbotAddianteSite1751912911775",
           {
-            scrt2URL: "https://addiante--devleoo1.sandbox.my.salesforce-scrt.com",
+            scrt2URL: "https://addiante.my.salesforce-scrt.com",
           }
         );
       } catch (err) {
