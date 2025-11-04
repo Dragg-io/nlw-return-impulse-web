6fcfe75010b47230029c917b085a135d719b3eb5
Lucas Pedro <lucas.pedro@leoo.com.br>
2025-11-04 10:54:25 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,7 @@ export function App() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://cdn.c360a.salesforce.com/beacon/c360a/a3af887d-712a-4a1a-843a-293d0d65420e/scripts/c360a.min.js";
-    script.defer = true;
-    script.async = true;
-    script.onload = () => {
-      try {
-        // @ts-ignore
-        window.embeddedservice_bootstrap.settings.language = "pt_BR";
-        // @ts-ignore
-        window.embeddedservice_bootstrap.init(
-          "00DHr000003YtPL",
-          "Chatbot_Addiante_Site",
-          "https://addiante.my.site.com/ESWChatbotAddianteSite1751912911775",
-          {
-            scrt2URL: "https://addiante.my.salesforce-scrt.com",
-          }
-        );
-      } catch (err) {
-        console.error("Erro ao carregar Embedded Messaging:", err);
-      }
-    };
+    script.type = "text/javascript";
     document.body.appendChild(script);
 
     return () => {
