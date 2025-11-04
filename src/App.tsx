2cc9e892f828cdd88f9232b3eca28c583d8d03c8
Lucas Pedro <lucas.pedro@leoo.com.br>
2025-11-04 17:10:31 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ export function App() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://cdn.c360a.salesforce.com/beacon/c360a/a3af887d-712a-4a1a-843a-293d0d65420e/scripts/c360a.min.js";
+    script.type = "text/javascript";
     document.body.appendChild(script);
- 
+
     return () => {
       // Opcional: remover o script ao sair da página, se necessário
       document.body.removeChild(script);
